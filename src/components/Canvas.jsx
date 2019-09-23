e8a8bd2e03982cbfd728d4776c452c6d8e3f5e9b
Manuel Duran <mduran85@gmail.com>
2019-09-23 14:39:04 -0500

    rendering multiple different items dynamically

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Item from './Item.jsx';
+import data from '../../exampleData.js';
+
 
 function Canvas() {
+  var items = data.businesses.map(ele => {
+    return <Item key={ele.id} business={ele}/>
+  })
   return (
     <div id="canvas-wrapper">
-      <Item/>
+      {items}
     </div>
   )
 }
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
-import data from '../../exampleData.js';
 
-function Item() {
-  var business = data.businesses[0];
-  var address = `${business.location.address1}, ${business.location.zip_code}, ${business.location.city}`;
+function Item(props) {
+  var address = `${props.business.location.address1}, ${props.business.location.zip_code}, ${props.business.location.city}`;
   return (
     <div id="item-wrapper">
       <div>
-        <img id="item-image" src={business.image_url}></img>
+        <img id="item-image" src={props.business.image_url}></img>
       </div>
       <div id="item-info">
         <ul>
-          <li id="item-name">{business.name}</li>
-          <li id="item-rating">{business.rating}</li>
-          <li id="item-categories">{business.categories[0].title}</li>
+          <li id="item-name">{props.business.name}</li>
+          <li id="item-rating">{props.business.rating}</li>
+          <li id="item-categories">{props.business.categories[0].title}</li>
           <li id="item-address">{address}</li>
         </ul>
       </div>
