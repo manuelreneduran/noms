18b17d2ff5d3f3e4feac1df23a2b9d9fbd44521c
Manuel Duran <mduran85@gmail.com>
2019-09-25 11:49:24 -0500

    reverted item back to presentational

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,35 +1,23 @@
 import React from 'react';
 
-class Item extends React.Component{
-
-  constructor(props) {
-    super(props);
-    this.state = {}
-  }
-
-
-render() {
+function Item(props) {
   var keyCount = 0;
   var address = `${props.business.location.address1} ${props.business.location.city} ${props.business.location.zip_code}`;
   var categories = props.business.categories.map(ele => { return <p key={keyCount++}>{ele.title}</p>})
   return (
-      <div id="item-wrapper">
-        <div>
-          <img id="item-image" src={props.business.image_url}></img>
-        </div>
-        <div id="item-info">
-          <div id="item-name-container"><p id="item-name">{props.business.name}</p></div>
-          <div id="item-rating-container"><p id="item-rating">{props.business.rating}</p></div>
-          <div id="item-categories-container">{categories}</div>
-          <div id="item-address-container"><p id="item-address">{address}</p></div>
-
-        </div>
+    <div id="item-wrapper">
+      <div>
+        <img id="item-image" src={props.business.image_url}></img>
       </div>
+      <div id="item-info">
+        <div id="item-name-container"><p id="item-name">{props.business.name}</p></div>
+        <div id="item-rating-container"><p id="item-rating">{props.business.rating}</p></div>
+        <div id="item-categories-container">{categories}</div>
+        <div id="item-address-container"><p id="item-address">{address}</p></div>
 
+      </div>
+    </div>
   )
-}
-
-
 }
 
 export default Item;
