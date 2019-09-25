f0ea3a0092b5fed85e296654328832a4660da80d
Manuel Duran <mduran85@gmail.com>
2019-09-25 11:25:10 -0500

    removed commas from address

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Item(props) {
   var keyCount = 0;
-  var address = `${props.business.location.address1}, ${props.business.location.zip_code}, ${props.business.location.city}`;
+  var address = `${props.business.location.address1} ${props.business.location.city} ${props.business.location.zip_code}`;
   var categories = props.business.categories.map(ele => { return <p key={keyCount++}>{ele.title}</p>})
   return (
     <div id="item-wrapper">
