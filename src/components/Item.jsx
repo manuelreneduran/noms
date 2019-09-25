7d99997f63ca1ffbda94e2b8586c2958b24e61ee
Manuel Duran <mduran85@gmail.com>
2019-09-25 11:37:59 -0500

    refactored Item component to functional

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,23 +1,35 @@
 import React from 'react';
 
-function Item(props) {
+class Item extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+
+render() {
   var keyCount = 0;
   var address = `${props.business.location.address1} ${props.business.location.city} ${props.business.location.zip_code}`;
   var categories = props.business.categories.map(ele => { return <p key={keyCount++}>{ele.title}</p>})
   return (
-    <div id="item-wrapper">
-      <div>
-        <img id="item-image" src={props.business.image_url}></img>
-      </div>
-      <div id="item-info">
-        <div id="item-name-container"><p id="item-name">{props.business.name}</p></div>
-        <div id="item-rating-container"><p id="item-rating">{props.business.rating}</p></div>
-        <div id="item-categories-container">{categories}</div>
-        <div id="item-address-container"><p id="item-address">{address}</p></div>
+      <div id="item-wrapper">
+        <div>
+          <img id="item-image" src={props.business.image_url}></img>
+        </div>
+        <div id="item-info">
+          <div id="item-name-container"><p id="item-name">{props.business.name}</p></div>
+          <div id="item-rating-container"><p id="item-rating">{props.business.rating}</p></div>
+          <div id="item-categories-container">{categories}</div>
+          <div id="item-address-container"><p id="item-address">{address}</p></div>
 
+        </div>
       </div>
-    </div>
+
   )
+}
+
+
 }
 
 export default Item;
