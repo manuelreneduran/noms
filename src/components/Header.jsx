8296178e492815531ff72c8a95e26ae920fe5def
Manuel Duran <mduran85@gmail.com>
2019-09-25 12:17:27 -0500

    refactored props to destructured

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-function Header(props) {
+function Header({handleSubmit, handleChange}) {
   return (
     <header id="header">
-      <form onSubmit={e => props.handleSubmit(e)}>
-        <input onChange={e => props.handleChange(e)} type="search" id="category-search" name="category" placeholder="Category"></input>
+      <form onSubmit={e => handleSubmit(e)}>
+        <input onChange={e => handleChange(e)} type="search" id="category-search" name="category" placeholder="Category"></input>
         {/* <input type="search" id="city-search" name="city" placeholder="City"></input> */}
         <button>Search</button>
       </form>
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
-function Item(props) {
+function Item({ business }) {
   var keyCount = 0;
-  var address = `${props.business.location.address1} ${props.business.location.city} ${props.business.location.zip_code}`;
-  var categories = props.business.categories.map(ele => { return <p key={keyCount++}>{ele.title}</p>})
+  var address = `${business.location.address1} ${business.location.city} ${business.location.zip_code}`;
+  var categories = business.categories.map(ele => { return <p key={keyCount++}>{ele.title}</p>})
   return (
     <div id="item-wrapper">
       <div>
-        <img id="item-image" src={props.business.image_url}></img>
+        <img id="item-image" src={business.image_url}></img>
       </div>
       <div id="item-info">
-        <div id="item-name-container"><p id="item-name">{props.business.name}</p></div>
-        <div id="item-rating-container"><p id="item-rating">{props.business.rating}</p></div>
+        <div id="item-name-container"><p id="item-name">{business.name}</p></div>
+        <div id="item-rating-container"><p id="item-rating">{business.rating}</p></div>
         <div id="item-categories-container">{categories}</div>
         <div id="item-address-container"><p id="item-address">{address}</p></div>
 
