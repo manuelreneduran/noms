6426d03c77b4e2817419c14ef11e4738a31d02ac
Manuel Duran <mduran85@gmail.com>
2019-09-26 10:04:58 -0500

    styled header and title, added logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 function Header({ handleSubmit, handleChange }) {
   return (
     <header id="header">
-      <form onSubmit={e => handleSubmit(e)}>
-        <input onChange={e => handleChange(e)} type="search" id="category-search" name="category" placeholder="Category"></input>
+      <form onSubmit={e => handleSubmit(e)} id="search-form">
+        <input onChange={e => handleChange(e)} type="search" id="category-search" name="category" placeholder="What do you want to eat?"></input>
         {/* <input type="search" id="city-search" name="city" placeholder="City"></input> */}
         <button>Search</button>
       </form>
