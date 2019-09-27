4fef878e089789c8cce383a3cfb81d1a39f7578a
Manuel Duran <mduran85@gmail.com>
2019-09-27 16:01:01 -0500

    added review link

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -9,7 +9,6 @@ function Item({ business, setActiveBusiness }) {
   var imageDivStyle = {
     backgroundImage: `url(${business.image_url})`
   };
-  var phone = business.phone.slice(1);
   return (
     <div id="item-wrapper">
       <div id="item-image-container" style={imageDivStyle}>
@@ -18,14 +17,21 @@ function Item({ business, setActiveBusiness }) {
       <div id="item-info">
         <div id="item-container-top">
           <div id="item-name-container"><p id="item-name">{business.name}</p></div>
-          <div id="item-rating-container"><StarRatings id="ratings" starDimension="15px" rating={business.rating}
-          starRatedColor="red" numberOfStars={5}/></div>
+          <div id="item-rating-container">
+            <StarRatings id="ratings" starDimension="15px" rating={business.rating}
+          starRatedColor="red" numberOfStars={5}/>
+            <div id="reviews">
+              <a href={business.url}>
+                <p>Reviews: {business.review_count}</p>
+              </a>
+              </div>
+          </div>
         </div>
 
         <div id="item-categories-container">{categories}</div>
         <div id="item-address-container"><a className="link" onClick={e => setActiveBusiness(business.name,
           business.coordinates.latitude, business.coordinates.longitude)} href="#" id="item-address">{address}</a></div>
-        <div id="item-phone-container"><p>{phone}</p></div>
+        <div id="item-phone-container"><p>{business.display_phone}</p></div>
         {business.is_closed ? <p id="isClosed">Closed</p> : <p id="isOpen">Open Now!</p>}
 
       </div>
