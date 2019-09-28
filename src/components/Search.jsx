da706cd9dc24ab93fa7d4caf210667c4eaf7431b
Manuel Duran <mduran85@gmail.com>
2019-09-28 09:19:52 -0500

    added location search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,8 +4,12 @@ function Search({ handleSubmit, handleChange }) {
   return (
     <div id="search-wrapper">
       <form onSubmit={e => handleSubmit(e)} id="search-form">
-        <input onChange={e => handleChange(e)} type="search" id="category-search" name="category" placeholder="What do you want to eat?"></input>
-        {/* <input type="search" id="city-search" name="city" placeholder="City"></input> */}
+        <div id="category-search-wrapper">
+          <input onChange={e => handleChange(e)} type="search" id="category-search" name="category" placeholder="What are you hungry for?"></input>
+        </div>
+        <div id="location-search-wrapper">
+          <input onChange={e => handleChange(e)} type="search" id="location-search" name="location" placeholder="City"></input>
+        </div>
         <button id="search-button"><i className="fas fa-search"></i></button>
       </form>
     </div>
